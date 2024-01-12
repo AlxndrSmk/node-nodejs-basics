9130f6cbdeb23266431dae730062b2b04409e882
Alexander Samak <voprosy262@gmail.com>
2024-01-12 22:54:29 +0300

    feat: add solution for 'copy.js'

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+
+const sourceDir = './files';
+const copyDir = './files_copy';
+
+const successMessage = `Folder '${
+  sourceDir.split('/')[1]
+}' succesfully copied to '${copyDir.split('/')[1]}' folder`;
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.cp(sourceDir, copyDir, {
+      force: false,
+      recursive: true,
+      errorOnExist: true,
+    });
+    console.log(successMessage);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
