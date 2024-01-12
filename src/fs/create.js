321de9ac6a4ea0fda386458a0a7296289d5feb10
Alexander Samak <voprosy262@gmail.com>
2024-01-12 20:31:06 +0300

    feat: add solution for 'create.js'

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
+
 const create = async () => {
-    // Write your code here 
+  const content = 'I am fresh and young';
+  const baseBath = './files';
+  const fileName = 'fresh.txt';
+  const alreadyExists = fs.existsSync(`${baseBath}/${fileName}`);
+
+  if (!alreadyExists) {
+    fs.writeFile(`${baseBath}/${fileName}`, content, (err) => {
+      if (err) throw err;
+    });
+    console.log(`File '${fileName}' created!`);
+  } else {
+    throw Error('FS operation failed');
+  }
 };
 
-await create();
+create();
