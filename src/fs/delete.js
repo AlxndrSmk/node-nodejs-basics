dc534cb55c7c80b9d3fe1a93bd697e3fcfb759da
Alexander Samak <voprosy262@gmail.com>
2024-01-14 23:32:44 +0300

    feat: add solution for 'delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
+const fileToDelete = './files/fileToRemove.txt';
+const successMessage = `File '${
+  fileToDelete.split('/')[2]
+}' was successfully deleted.`;
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    fs.unlink(fileToDelete, (err) => {
+      if (err) {
+        throw new Error('FS operation failed');
+      }
+      console.log(successMessage);
+    });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
