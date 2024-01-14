e24616392352b43c639770fa66b2ce6793ea9a6e
Alexander Samak <voprosy262@gmail.com>
2024-01-14 23:24:18 +0300

    feat: add solution for 'rename.js'

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import * as fsPromises from 'fs/promises';
+import fs from 'fs';
+
+const wrongFile = './files/wrongFilename.txt';
+const properFile = './files/properFilename.md';
+
+const successMessage = `File '${
+  wrongFile.split('/')[2]
+}' was succesfully renamed to '${properFile.split('/')[2]}'`;
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    if (!fs.existsSync(wrongFile) || fs.existsSync(properFile)) {
+      throw new Error('FS operation failed');
+    }
+    await fsPromises.rename(wrongFile, properFile);
+    console.log(successMessage);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
