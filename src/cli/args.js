f3e98872af26fe6c71ff98a8cea42f25934d3319
Alexander Samak <voprosy262@gmail.com>
2024-01-16 01:23:50 +0300

    feat: add solution for 'args.js'

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  if (process.argv.length === 2) {
+    console.error('Expected at least one argument!');
+    process.exit(1);
+  }
+
+  const args = process.argv.slice(2);
+
+  args.map((el, i) => {
+    if (el.startsWith('--')) console.log(`${el.slice(2)} is ${args[i + 1]}`);
+  });
 };
 
 parseArgs();
