0405871e0d38a991695db74055499028d304afeb
Alexander Samak <voprosy262@gmail.com>
2024-01-20 21:24:59 +0300

    feat: add solution for 'write.js' file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
-const write = async () => {
-    // Write your code here 
+import { createWriteStream } from 'fs';
+
+const pathtToWrite = './files/fileToWrite.txt';
+
+export const write = async () => {
+  const streamFileToWrite = createWriteStream(pathtToWrite);
+
+  console.log('Enter text:');
+
+  process.stdin.on('data', (data) => {
+    streamFileToWrite.write(data.toString());
+
+    console.log(
+      `Entered text was successfully written into '${
+        pathtToWrite.split('/')[2]
+      }' file.`
+    );
+
+    process.exit();
+  });
 };
 
-await write();
+write();
