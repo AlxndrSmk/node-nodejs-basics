b942b33736922a7727817fad7d6b3de5c3aba65c
Alexander Samak <voprosy262@gmail.com>
2024-01-20 22:40:05 +0300

    feat: add solution for 'transform.js' file

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'stream';
+import { EOL } from 'os';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseText = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(
+        chunk.toString().replace(EOL, '').split('').reverse().join('') + '\n'
+      );
+      callback();
+    },
+  });
+
+  process.stdin.pipe(reverseText).pipe(process.stdout);
 };
 
 await transform();
