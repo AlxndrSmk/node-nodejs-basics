02474e357d3dc8caf98dc09a9b7402e9d0212517
Alexander Samak <voprosy262@gmail.com>
2024-01-21 14:06:31 +0300

    feat: add solution for 'decompress.js' task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    fs.createReadStream('./files/archive.gz')
+      .pipe(zlib.createGunzip())
+      .pipe(fs.createWriteStream('./files/fileToCompress.txt'));
+
+    console.log('File decompressed.');
+  } catch (err) {
+    console.error('An error occurred:', err);
+    process.exitCode = 1;
+  }
 };
 
 await decompress();
